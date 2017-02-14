aace534e98f8483addfc7a7c6e73228c8dd0d2b9
newzpanuwat <pfcreatorit@gmail.com>
2017-02-14 23:18:47 +0700

    Findstack

--- src/stackPro.java
+++ src/stackPro.java
@@ -1,11 +1,39 @@
 import java.util.*;
 public class stackPro {
-    public static void main(String args[]){
+    public static void main(String args[]) {
+
+        popStack();
+
+    }
+
+    public static void popStack() {
         Stack<String> st = new Stack<>();
         st.push("Thai");
         st.push("Laos");
         st.push("Vietnam");
+        st.push("Cambodia");
+        st.push("Singapore");
 
-        System.out.println(st);
+        while (!st.empty()) {
+            for(int i = 1;i<=6; i++){
+
+            }
+            System.out.println(st.pop());
+        }
+
+        System.out.println("Out of stack");
+
+    }
+
+    public static void searchStack(){
+        Stack<String> st = new Stack<>();
+        st.push("Chealsea");
+        st.push("Liverpool");
+        st.push("ManCity");
+        st.push("Arsenal");
+        st.push("Norwich");
+
+        System.out.println(st.peek());
+
     }
 }
