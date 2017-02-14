bb4e927058fbe77b3cd5abba3a13cc4c7d9eaa38
newzpanuwat <pfcreatorit@gmail.com>
2017-02-14 22:13:49 +0700

    Stack javaDatastructure

---
+++ src/stackPro.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class stackPro {
+    public static void main(String args[]){
+        Stack<String> st = new Stack<>();
+        st.push("Thai");
+        st.push("Laos");
+        st.push("Vietnam");
+
+        System.out.println(st);
+    }
+}
